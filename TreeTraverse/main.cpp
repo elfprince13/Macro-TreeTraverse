e2d0e88392860933a76af0d652cccb2df25f97cf
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2015-03-13 00:34:54 -0400

    Initial Commit

---
+++ TreeTraverse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TreeTraverse
+//
+//  Created by Thomas Dickerson on 3/13/15.
+//  Copyright (c) 2015 StickFigure Graphic Productions. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
